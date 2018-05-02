d121ef3d25b862909c6ae5d2799e411c18755562
lengyeudaidai <418088181@qq.com>
2018-05-02 15:42:53 +0800

    增加DAO构建框架

---
+++ src/test/java/com/dai/daobuild/test/build/render/RenderParam.java
@@ -0,0 +1,60 @@
+package com.dai.daobuild.test.build.render;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.dai.daobuild.test.build.entity.FileInfo;
+import com.dai.daobuild.test.build.entity.TableInfo;
+
+/**
+ * @author Dai 渲染参数
+ *
+ */
+public class RenderParam {
+	/*
+	 * private FileInfo modelClassFile; private FileInfo mapperClassFile;
+	 * private FileInfo mapperXMLFile; private FileInfo serviceClassFile;
+	 * private FileInfo serviceImplClassFile;
+	 */
+	private TableInfo table;
+	private Map<String, FileInfo> fileInfoMap = new HashMap<String, FileInfo>();
+
+	public FileInfo getFileInfo(String name) {
+		if (fileInfoMap.containsKey(name)) {
+			return fileInfoMap.get(name);
+		} else {
+			return null;
+		}
+	}
+
+	public void addFileInfo(String name, FileInfo fileInfo) {
+		fileInfoMap.put(name, fileInfo);
+	}
+
+	public HashMap<String, Object> toHashMap()
+			throws IllegalArgumentException, IllegalAccessException {
+		HashMap<String, Object> hm = new HashMap<String, Object>();
+		Field[] fields = this.getClass().getDeclaredFields();
+		for (Field field : fields) {
+			hm.put(field.getName(), field.get(this));
+		}
+		return hm;
+	}
+
+	/**
+	 * @return table
+	 */
+	public TableInfo getTable() {
+		return table;
+	}
+
+	/**
+	 * @param table
+	 *            要设置的 table
+	 */
+	public void setTable(TableInfo table) {
+		this.table = table;
+	}
+
+}
---
+++ src/test/java/com/dai/daobuild/test/dao/UserMapperTest.java
@@ -0,0 +1,24 @@
+package com.dai.daobuild.test.dao;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+
+import com.dai.jigsaw.core.feature.test.TestSupport;
+import com.dai.jigsaw.web.dao.UserMapper;
+
+public class UserMapperTest extends TestSupport {
+/*	@Resource
+	private UserMapper userMapper;
+
+	@Test
+	public void test_selectByExampleAndPage() {
+		
+		 * start(); Page<User> page = new Page<>(1, 3); UserExample example =
+		 * new UserExample(); example.createCriteria().andIdGreaterThan(0L);
+		 * final List<User> users = userMapper.selectByExampleAndPage(page,
+		 * example); for (User user : users) { System.err.println(user); }
+		 * end();
+		 
+	}*/
+}
